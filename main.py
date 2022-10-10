9e3dace9729d465b4d4972b70234e59631dc98d1
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 02:32:57 -0700

    Chap7 Lab3

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
+# List for 5 int values
+numbers = []
 
-# ******************************
-# Make your Code
-# ******************************
+# List for values > avg of numbers
+greater = []
+
+# Get 5 int values and add to list
+print("Enter 5 integer values: ")
+for i in range(5):
+    numbers.append(int(input()))
+
+# Get avg of list sum
+avg = sum(numbers) // 5
+
+# Find elements in list > avg
+for i in range(5):
+    if numbers[i] > avg:
+        great = numbers[i]
+        greater.append(great)
+
+# Print numbers/greater list
+print("List:", end = ' ')
+for i in range(5):
+    print(numbers[i], end = ' ')
+print("\nAverage of list:", avg)
+
+print("Greater than average:", end = ' ')
+j = 0
+for j in range(2):
+    print(greater[j], end = ' ')
